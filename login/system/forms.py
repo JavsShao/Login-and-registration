258095649326b4c8f444eefd3815e490c86f9e6a
hamdi <javs_shao@163.com>
2019-01-13 17:33:54 +0800

    ModelForm

--- login/system/forms.py
+++ login/system/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from . import models
 
 
 class UserForm(forms.Form):
-    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = models.User
+        fields = ['name', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, *kwargs)
+        self.fields['name'].lable = '用户名'
+        self.fields['password'].lable = '密码'
