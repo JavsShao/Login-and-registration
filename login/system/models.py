557ad29d954382351e15c7e4a465a18e6c592092
hamdi <javs_shao@163.com>
2019-01-14 12:02:42 +0800

    发送邮件_编写模型

--- login/system/models.py
+++ login/system/models.py
@@ -25,3 +25,16 @@ class User(models.Model):
         ordering = ["-c_time"]
         verbose_name = "用户"
         verbose_name_plural = "用户"
+
+class ConfirmString(models.Model):
+    code = models.CharField(max_length=256)
+    user = models.OneToOneField('user')
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.name + ":  " + self.code
+
+    class Meta:
+        ordering = ['-c_time']
+        verbose_name = "确认码"
+        verbose_name_plural = "确认码"
