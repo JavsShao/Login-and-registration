36155adc44c039231c7af4c53a1d830e0ebbeaea
hamdi <javs_shao@163.com>
2019-01-14 11:55:00 +0800

    测试邮件_代码优化

--- login/send_mail.py
+++ login/send_mail.py
@@ -1,14 +1,13 @@
 import os
-from django.core.mail import send_mail
-
+from django.core.mail import send_mail, EmailMultiAlternatives
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'login.settings'
 
 if __name__ == '__main__':
 
-    send_mail(
-        '这是测试邮件',
-        '来自：Hamdi',
-        'javs_shao@163.com',
-        ['shaoleei@126.com'],
-    )
+    subject, from_email, to = '来自www.Hamdi.com的测试邮件', 'javs_shao@163.com', 'shaoleei@126.com'
+    text_content = "欢迎访问www.baidu.com, 这里是百度的首页!"
+    html_content = '<p>欢迎访问<a href="http://www.baidu.com" target=blank>www.liujiangblog.com</a>，这里是百度的首页，全球最大的中文搜索引擎!</p>'
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
