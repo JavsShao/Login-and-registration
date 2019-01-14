3eb0ba345cad2c48495b43b8873e6fd427470b62
hamdi <javs_shao@163.com>
2019-01-14 11:45:57 +0800

    测试发送邮件_1

--- login/send_mail.py
+++ login/send_mail.py
@@ -7,8 +7,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'login.settings'
 if __name__ == '__main__':
 
     send_mail(
-        '来自www.liujiangblog.com的测试邮件',
-        '欢迎访问www.liujiangblog.com，这里是刘江的博客和教程站点，本站专注于Python和Django技术的分享！',
+        '这是测试邮件',
+        '来自：Hamdi',
         'javs_shao@163.com',
         ['shaoleei@126.com'],
     )
