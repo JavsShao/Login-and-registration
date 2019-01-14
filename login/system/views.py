0e145a216351f556d17e05bbdbfedea36347322a
hamdi <javs_shao@163.com>
2019-01-14 11:28:12 +0800

    Hash密码加密_修改login函数 和 register函数

--- login/system/views.py
+++ login/system/views.py
@@ -12,74 +12,29 @@ def index(request):
     return render(request, 'login/index.html')
 
 def login(request):
-    """
-    登录视图
-    :param request:
-    :return:
-    """
-    # 不允许重复登录
     if request.session.get('is_login', None):
-        return redirect('/index')
+        return redirect("/index/")
     if request.method == "POST":
         login_form = forms.UserForm(request.POST)
-        message = "用户名和密码都必须填写！"
+        message = "请检查填写的内容！"
         if login_form.is_valid():
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
             try:
                 user = models.User.objects.get(name=username)
-                if user.password == password:
+                if user.password == hash_code(password):  # 哈希值和数据库内的值进行比对
                     request.session['is_login'] = True
                     request.session['user_id'] = user.id
                     request.session['user_name'] = user.name
                     return redirect('/index/')
                 else:
-                    message = "密码不正确!"
+                    message = "密码不正确！"
             except:
-                message = "用户不存在!"
+                message = "用户不存在！"
         return render(request, 'login/login.html', locals())
+
     login_form = forms.UserForm()
     return render(request, 'login/login.html', locals())
-
-
-
-# def register(request):
-#     if request.session.get('is_login', None):
-#         # 登录状态下,不允许注册
-#         return redirect('/index/')
-#     if request.method == "POST":
-#         register_form = forms.RegisterForm(request.POST)
-#         message = "请检查填写的内容!"
-#         if register_form.is_valid():
-#             username = register_form.cleaned_data['username']
-#             password1 = register_form.cleaned_data['password1']
-#             password2 = register_form.cleaned_data['password2']
-#             email = register_form.cleaned_data['email']
-#             sex = register_form.cleaned_data['sex']
-#             if password1 != password2:
-#                 message = "两次输入的密码不相同！"
-#                 return render(request, 'login/register.html', locals())
-#             else:
-#                 same_name_user = models.User.objects.filter(name=username)
-#                 if same_name_user:  # 已经存在此用户名
-#                     message = "当前用户名已经存在, 请重新选择用户名!"
-#                     return render(request, 'login/register.html', locals())
-#                 same_email_user = models.User.objects.filter(email=email)
-#                 if same_email_user: # 邮件地址唯一
-#                     message = "当前邮件地址已经被注册, 请使用别的邮箱!"
-#                     return render(request, 'login/register.html', locals())
-#
-#
-#                 new_user = models.User()
-#                 new_user.name = username
-#                 new_user.password = password1
-#                 new_user.email = email
-#                 new_user.sex = sex
-#                 new_user.save()
-#                 return redirect('/login/')  # 自动跳转到登录页面
-#         register_form = forms.RegisterForm()
-#         return render(request, 'login/register.html', locals())
-
 
 
 def register(request):
@@ -112,7 +67,7 @@ def register(request):
 
                 new_user = models.User()
                 new_user.name = username
-                new_user.password = password1
+                new_user.password = hash_code(password1)  # 使用加密密码
                 new_user.email = email
                 new_user.sex = sex
                 new_user.save()
